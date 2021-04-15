4c5d10d69d8ab19550367f00172b4ea1224a0696
ocawthorne <ocawthorne@gmail.com>
2021-04-15 17:42:31 +0100

    Produce outputted encryption to DOM

--- src/Encoder.js
+++ src/Encoder.js
@@ -8,35 +8,6 @@ export default class Encoder extends React.Component {
       encodedMessage: ""
    }
 
-   library = {
-      a: 1,
-      b: 2,
-      c: 3,
-      d: 4,
-      e: 5,
-      f: 6,
-      g: 7,
-      h: 8,
-      i: 9,
-      j: 10,
-      k: 11,
-      l: 12,
-      m: 13,
-      n: 14,
-      o: 15,
-      p: 16,
-      q: 17,
-      r: 18,
-      s: 19,
-      t: 20,
-      u: 21,
-      v: 22,
-      w: 23,
-      x: 24,
-      y: 25,
-      z: 26
-   }
-
    phraseEntry = e => {
       this.setState({
          ...this.state,
@@ -54,15 +25,29 @@ export default class Encoder extends React.Component {
    performEncode = () => {
       let encoderArray = []
       let phrase = this.state.phrase.toLowerCase().split("")
-      phrase.forEach(letter => {
-         encoderArray.push(this.library[letter])
-      });
+      phrase.forEach(letter => encoderArray.push(letter.charCodeAt(0)-96));
       this.setState({
          ...this.state,
          encoder: encoderArray
       })
       setTimeout(() => {
-         console.log(this.state)
+         let message = this.state.message.toLowerCase().split("")
+         let phraseArray = []
+
+         
+         for (let i = 0; i < message.length; i++) {
+            // console.log(message[i].charCodeAt(0))
+            // console.log(parseInt(this.state.encoder[i % this.state.encoder.length]))
+            phraseArray.push((parseInt(message[i].charCodeAt(0)) + parseInt(this.state.encoder[i % this.state.encoder.length]) - 97) % 26)
+         }
+         
+         this.setState({
+            ...this.state,
+            encodedMessage: phraseArray.map(a => String.fromCharCode(a+97)).join("")
+         })
+
+         console.log(this.state.encodedMessage.length - this.state.message.length)
+
       }, 2);
    }
 
